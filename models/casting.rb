75e82e11ca7aa8f97c8f6b0a6f588c5b9b62ba69
bchutchison <benhutchison85@gmail.com>
2019-07-11 14:58:01 +0100

    Adds save casting method

--- models/casting.rb
+++ models/casting.rb
@@ -15,4 +15,19 @@ def initialize(options)
   @fee = options['fee'].to_i
 end
 
+def save()
+  sql = "INSERT INTO castings
+  (
+  movie_id, star_id, fee
+  )
+  VALUES
+  (
+    $1, $2, $3
+  )
+  RETURNING id"
+  values = [@movie_id, @star_id, @fee]
+  casting = SqlRunner.run( sql, values ).first
+  @id = casting['id'].to_i
+end
+
 end
