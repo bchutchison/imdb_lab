30ca541f377bc6b5ba9f7afd201b057a1564aa81
bchutchison <benhutchison85@gmail.com>
2019-07-11 14:12:27 +0100

    movie.rb setup

--- models/movie.rb
+++ models/movie.rb
@@ -1,5 +1,15 @@
+require_relative("../db/sql_runner.db")
+
 class Movie
 
-  
+attr_reader :id
+attr_accessor :title, :genre
+
+def initialize( options )
+@id = options['id'].to_i if options['id']
+@title = options['title']
+@genre = options['genre']
+end
+
 
 end
